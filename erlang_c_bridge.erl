1d11c3193fd07dca4134351a7c5d452eb915d9be
Santiago Salvatico <san.salvatico@gmail.com>
2026-06-11 22:05:56 +0000

    Progress on module erlang_bridge_c

--- erlang_c_bridge.erl
+++ erlang_c_bridge.erl
@@ -2,37 +2,124 @@
 -include("header.hrl").
 
 
-%% Esta mal, tengo que hacer que la funcion se conecte al agente C
-%% El responsable de abrir el server es el agente, no yo
-server(ServLogger) ->
-    case gen_tcp:listen(?PORT,[binary, {active, false}]) of
-    {ok, Socket} ->
-        ServLogger ! #logInfo{status = ok,
-                              src_module = server,
-                              detail = none,
-                              job_involved = self(),
-                              timestamp = calendar:local_time()
-                            },
-        spawn(?MODULE, conn_handler, [Socket, ServLogger]);
+%%% Connect tiene la responsabilidad de conectarse al agente C
+%%% en ?HOST:?PORT.
+%%% Una vez conectado, notifica al proceso logger, 
+%%% crea el proceso conn_handler y termina.
+%%% Para evitar problemas, connect tiene 3 intentos para una conexion
+%%% exitosa. En caso de error, notifica al logger y termina.
+connect(ServLoggerId, 0, JobSchedulerId) ->
+    ServLoggerId ! #logInfo{status = error,
+                            src_method = {?MODULE, ?FUNCTION_NAME},
+                            detail = "Out of tries.",
+                            job_involved = none,
+                            timestamp = ?TIMESTAMP
+                          },
+    JobSchedulerId ! {error, "Out of tries"};
+connect(ServLoggerId, Nth_try, JobSchedulerId) ->
+    case gen_tcp:connect(?HOST , ?PORT , [binary, {active, false}] , ?TIMEOUT) of
+        {ok, Socket} ->
+            ServLoggerId ! #logInfo{status = ok,
+                                    src_method = {?MODULE, ?FUNCTION_NAME},
+                                    detail = "Connection succesful.",
+                                    job_involved = none,
+                                    timestamp = ?TIMESTAMP
+                                },
+            spawn(?MODULE, conn_handler, [ServLoggerId, Socket, JobSchedulerId]);
     
-    {error, Reason} -> 
-        ServLogger ! #logInfo{status = error,
-                              src_module = server,
-                              detail = Reason,
-                              job_involved = self(),
-                              timestamp = calendar:local_time()
-                            }
+        {error, Reason} -> 
+            ServLoggerId ! #logInfo{status = error,
+                                    src_method = {?MODULE, ?FUNCTION_NAME},
+                                    detail = Reason,
+                                    job_involved = none,
+                                    timestamp = ?TIMESTAMP
+                                },
+                                connect(ServLoggerId, Nth_try - 1, JobSchedulerId)
+    end.
+
+conn_handler(ServLoggerId, Socket, JobSchedulerId) ->
+    ReceiverId = spawn(?MODULE, receiver, [ServLoggerId, Socket, JobSchedulerId]),
+    SenderId = spawn(?MODULE, sender, [ServLoggerId, Socket, JobSchedulerId]),
+    JobSchedulerId ! {ok, ReceiverId, SenderId}.
+
+%%% C -> RESPONSE -> receiver -> scheduler
+receiver(ServLoggerId, Socket, JobSchedulerId) ->
+    case gen_tcp:recv(Socket, 0, ?TIMEOUT) of
+        {ok, Packet} ->
+            JobSchedulerId ! {ok, Packet},
+            ServLoggerId ! #logInfo{status = ok,
+                                    src_method = {?MODULE, ?FUNCTION_NAME},
+                                    detail = "C response.",
+                                    job_involved = none,
+                                    timestamp = ?TIMESTAMP
+                                },
+                                receiver(ServLoggerId, Socket, JobSchedulerId);
+        {error, closed} ->
+            JobSchedulerId ! {error, closed},
+            ServLoggerId ! #logInfo{status = error,
+                                    src_method = {?MODULE, ?FUNCTION_NAME},
+                                    detail = closed,
+                                    job_involved = none,
+                                    timestamp = ?TIMESTAMP
+                                };
+        {error, Reason} ->
+            JobSchedulerId ! {error, Reason},
+            ServLoggerId ! #logInfo{status = error,
+                                    src_method = {?MODULE, ?FUNCTION_NAME},
+                                    detail = Reason,
+                                    job_involved = none,
+                                    timestamp = ?TIMESTAMP
+                                }
     end,
     ok.
 
-conn_handler(Socket, ServLogger) ->
-    case gen_tcp:accept(Socket) of
-        {ok, ClientSocket} -> 
-            
-            spawn(?MODULE, packet_handler, [ClientSocket]);
-        {error, closed} -> 
-            ServLogger !
+%%% scheduler -> REQUEST -> sender -> C
+sender(ServLoggerId, Socket, JobSchedulerId) ->
+    receive
+        {get_nodes} ->
+            case gen_tcp:send(Socket, <<"GET_NODES">>) of
+                ok ->
+                    ServLoggerId ! #logInfo{status = ok,
+                                            src_method = {?MODULE, ?FUNCTION_NAME},
+                                            detail = get_nodes,
+                                            job_involved = none,
+                                            timestamp = ?TIMESTAMP
+                                        },
+                    JobSchedulerId ! {ok, get_nodes};
+                {error, Reason} ->
+                    ServLoggerId ! #logInfo{status = error,
+                                            src_method = {?MODULE, ?FUNCTION_NAME},
+                                            detail = Reason,
+                                            job_involved = none,
+                                            timestamp = ?TIMESTAMP
+                                        },
+                    JobSchedulerId ! {error, get_nodes}
+            end;
+        % Here we cover the cases JOB_REQUEST and JOB_RELEASE
+        {job_directive, JobId, Packet} ->
+            case gen_tcp:send(Socket, Packet) of
+                ok ->
+                    ServLoggerId ! #logInfo{status = ok,
+                                            src_method = {?MODULE, ?FUNCTION_NAME},
+                                            detail = job_directive,
+                                            job_involved = JobId,
+                                            timestamp = ?TIMESTAMP
+                                        },
+                    JobSchedulerId ! {ok, JobId, job_directive};
+                {error, Reason} ->
+                    ServLoggerId ! #logInfo{status = error,
+                                            src_method = {?MODULE, ?FUNCTION_NAME},
+                                            detail = Reason,
+                                            job_involved = JobId,
+                                            timestamp = ?TIMESTAMP
+                                        },
+                    JobSchedulerId ! {error, JobId, job_directive}
+            end
     end,
-    ok.
+    sender(ServLoggerId, Socket, JobSchedulerId).
 
-init() -> ok.
+
+init() -> 
+    ServLoggerId = spawn(logger, init, []),
+    JobSchedulerId = spawn(job_scheduler, init, []),
+    connect(ServLoggerId, ?TRIES, JobSchedulerId).
