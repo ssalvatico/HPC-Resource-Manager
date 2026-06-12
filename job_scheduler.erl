db8a0b60b11df8aceb37dfd11a19b7acbd563e82
Camilunchis <batscumbasa0@gmail.com>
2026-06-12 17:17:45 -0300

    Send different messages to job_scheduler

--- job_scheduler.erl
+++ job_scheduler.erl
@@ -1,3 +1,13 @@
 -module(job_scheduler).
 -include("header.hrl").
+-export([init/0]).
 
+init() ->
+  receive
+    {receiver_pid, ReceiverId} ->
+      io:fwrite("receiver_pid received"),
+      init();
+    {sender_pid, SenderPid} ->
+      io:fwrite("sender_pid received"),
+      init()
+  end.
