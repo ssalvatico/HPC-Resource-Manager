f8125d5fbaad8b4e3c6439058248b8441fbef70a
Camilunchis <batscumbasa0@gmail.com>
2026-06-13 12:51:02 -0300

    Added functions to handle packages

--- job_scheduler.erl
+++ job_scheduler.erl
@@ -1,5 +1,6 @@
 -module(job_scheduler).
 -include("header.hrl").
+-import(event_logger, [log_event/5]).
 -export([init/0, get_node_list/1]).
 
 init() ->
@@ -31,3 +32,48 @@ get_node_list(Packet) ->
   String = binary:bin_to_list(Packet),
   TrimedString = string:trim(String),
   string:split(TrimedString, ";", all).
+
+
+handle_packet(<<"NODES ", Rest/binary>>) ->
+    NodeList = get_node_list(Rest),
+    NodeRecordList = lists:map(fun get_node/1, NodeList),
+    io:fwrite("Node Record List ~p ~n", [NodeRecordList]);
+  
+handle_packet(<<"JOB_GRANTED ", Rest/binary>>) ->
+    ok;
+
+handle_packet(<<"JOB_DENIED ", Rest/binary>>) ->
+    ok;
+
+handle_packet(<<"JOB_TIMEOUT ", Rest/binary>>) ->
+    ok.
+
+
+
+%% Auxiliar Functions to get resources from each Node.
+
+get_node(Node) ->
+    NodeRecord = #node{},
+    [Ip, Port | Resources] = string:split(Node, ":", all),
+    % [{"cpu", 4}, {"gpu", 10}]
+    ResourcePairs = to_pairs(Resources),
+    NodeRecord2 = lists:foldl(fun assign_resource/2 ,NodeRecord,ResourcePairs),
+    NodeRecord2#node{ip = Ip, port = Port}.
+
+    
+to_pairs([Key, Value | Rest]) ->
+    [{Key, list_to_integer(Value)} | to_pairs(Rest)];
+to_pairs([]) ->
+    [].
+
+assign_resource({"cpu", Amount}, NodeRecord) ->
+    NodeRecord#node{cpu = Amount};
+assign_resource({"gpu", Amount}, NodeRecord) ->
+    NodeRecord#node{gpu = Amount}; 
+assign_resource({"mem", Amount}, NodeRecord) ->
+    NodeRecord#node{mem = Amount}.
+
+
+%%
+
+
